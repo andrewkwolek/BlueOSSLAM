550f12b38c53faa20cfa1a1bd1ef6b2c277b0588
Andrew Kwolek <andrewkwolek2025@u.northwestern.edu>
2025-02-10 15:12:35 -0600

    Get proper ping data

--- app/PingManager.py
+++ app/PingManager.py
@@ -24,17 +24,20 @@ class PingManager():
             logger.error("Failed to initialize Ping!")
 
     async def get_ping_data(self):
-        while self.gathering:
-            data = self.my_ping.get_auto_device_data()
-            sonar_data = SonarData(
-                angle=data['angle'],
-                transmit_duration=data['transmit_duration'],
-                sample_period=data['sample_period'],
-                start_angle=data['start_angle'],
-                stop_angle=data['stop_angle'],
-                number_of_samples=data['number_of_samples'],
-                data_length=data['data_length'],
-                data=data['data']
-            )
+        data = self.my_ping.get_auto_device_data()
+        sonar_data = SonarData(
+            angle=data['angle'],
+            transmit_duration=data['transmit_duration'],
+            sample_period=data['sample_period'],
+            start_angle=data['start_angle'],
+            stop_angle=data['stop_angle'],
+            number_of_samples=data['number_of_samples'],
+            data_length=data['data_length'],
+            data=data['data']
+        )
 
-            return sonar_data
+        return sonar_data
+
+    async def gather_ping_data(self):
+        while self.gathering:
+            return self.get_ping_data()
