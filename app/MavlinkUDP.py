83724da10be5d69b5b3bd7d571ce21e9073d39cb
Andrew Kwolek <andrewkwolek2025@u.northwestern.edu>
2025-02-11 16:20:15 -0600

    Didn't pass DataManager

--- app/MavlinkUDP.py
+++ app/MavlinkUDP.py
@@ -3,22 +3,22 @@ from loguru import logger
 
 
 class MavlinkUDPProtocol(DatagramProtocol):
-    def __init__(self):
-        self.transport = None
+    def __init__(self, data_manager):
+        self.data_manager = data_manager  # You can store data_manager to store data
 
     def connection_made(self, transport):
         self.transport = transport
-        logger.info(f"Connected to UDP server")
+        logger.info("UDP connection established")
 
     def datagram_received(self, data, addr):
-        # Handle incoming data (MAVLink messages or whatever your protocol is)
-        logger.debug(f"Received data: {data} from {addr}")
-        # You can now process this data, e.g., parse MAVLink messages
-        # Example: Print data
-        print(f"Received {data} from {addr}")
+        """ This method is called when a UDP packet is received """
+        logger.debug(f"Received UDP packet from {addr}")
+
+        # You can store the data in your data manager or any other class
+        # self.data_manager.process_udp_data(data, addr)
 
     def error_received(self, exc):
-        logger.error(f"Error received: {exc}")
+        logger.error(f"Error receiving data: {exc}")
 
     def connection_lost(self, exc):
-        logger.info("Connection lost")
+        logger.info("UDP connection lost")
