f7d3aa3968ae7b69f373d021ee5bb630be679848
Andrew Kwolek <andrewkwolek2025@u.northwestern.edu>
2025-02-11 17:56:17 -0600

    Changed listener

--- app/MavlinkUDP.py
+++ app/MavlinkUDP.py
@@ -1,24 +1,40 @@
-from asyncio import DatagramProtocol
+from pymavlink import mavutil
 from loguru import logger
 
 
-class MavlinkUDPProtocol(DatagramProtocol):
+class MavlinkUDPProtocol:
     def __init__(self, data_manager):
-        self.data_manager = data_manager  # You can store data_manager to store data
+        self.data_manager = data_manager
+        # Using None for now, will parse raw data
+        self.mav = mavutil.mavlink_connection('udp:host.docker.internal:14550')
 
     def connection_made(self, transport):
         self.transport = transport
         logger.info("UDP connection established")
 
     def datagram_received(self, data, addr):
-        """ This method is called when a UDP packet is received """
-        logger.debug(f"Received UDP packet from {addr}")
+        """
+        This method will be called when a UDP packet is received.
+        It parses the incoming MAVLink data and processes it.
+        """
+        try:
+            # The connection object expects the data in bytes. We can use `mavutil.mavlink_connection`
+            # to parse the raw MAVLink data from the UDP packet.
+            self.mav.parse(data)
 
-        # You can store the data in your data manager or any other class
-        # self.data_manager.process_udp_data(data, addr)
+            # Check if the message was valid
+            while self.mav.messages:
+                msg = self.mav.messages.pop(0)
+                logger.debug(f"Received MAVLink message: {msg}")
+
+                # Process and store the message (for example, storing in data_manager)
+                # self.data_manager.store_data(msg)
+
+        except Exception as e:
+            logger.error(f"Error processing MAVLink data: {e}")
 
     def error_received(self, exc):
-        logger.error(f"Error receiving data: {exc}")
+        logger.error(f"Error received: {exc}")
 
     def connection_lost(self, exc):
         logger.info("UDP connection lost")
