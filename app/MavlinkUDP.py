0dce3e8c7278fb56fd0c10855df516b15e8fff91
Andrew Kwolek <andrewkwolek2025@u.northwestern.edu>
2025-02-12 12:27:52 -0600

    Trying to get dummy connection

--- app/MavlinkUDP.py
+++ app/MavlinkUDP.py
@@ -9,19 +9,19 @@ from typedefs import MavlinkMessage
 class MavlinkUDPProtocol(DatagramProtocol):
     def __init__(self, data_processor: Processor):
         self.data_processor = data_processor  # You can store data_manager to store data
-        self.mav = mavutil.mavudp("")
+        self.mav = mavutil.mavudp("localhost:0")
 
     def connection_made(self, transport):
         self.transport = transport
         logger.info("UDP connection established")
 
-    def datagram_received(self, data, addr):
+    def datagram_received(self, raw_data, addr):
         """ This method is called when a UDP packet is received """
         logger.debug(f"Received UDP packet from {addr}")
 
         # You can store the data in your data manager or any other class
         # self.data_manager.process_udp_data(data, addr)
-        self.mav.recv(data)
+        self.mav.recv(raw_data)
         msg = self.mav.recv_match()
 
         if msg:
